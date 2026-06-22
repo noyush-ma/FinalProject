5efdd565f754d4f9f959c77097ffe1087e0158fb
ShirBenshoshan1 <shirushbs26@gmail.com>
2026-06-22 16:10:17 +0300

    searchBox update + classes
    searchBox update + classes + "no result" message
    uploading 12 more pictures to the site.

--- feed.js
+++ feed.js
@@ -15,6 +15,10 @@ function openModal(element) {
     document.getElementById("modalDesc").innerText = targetDesc;
 
     document.getElementById("heartIcon").src = "emptyHeart.png";
+
+    var saveBtn = document.getElementById("saveBtn");
+    saveBtn.innerText = "שמירה";
+    saveBtn.style.backgroundColor = "#e60023";
 }
 
 function closeModal() {
@@ -34,4 +38,43 @@ function doLike() {
         heartIcon.src = "emptyHeart.png";
         likeCount.innerText = currentLikes - 1;
     }
+
 }
+
+function doSave() {
+    var saveBtn = document.getElementById("saveBtn");
+    if (saveBtn.innerText === "שמירה") {
+        saveBtn.innerText = "נשמר";
+        saveBtn.style.backgroundColor = "#333333";
+    } else{
+        saveBtn.innerText = "שמירה";
+        saveBtn.style.backgroundColor = "#e60023";
+    }
+}
+
+function postsText() {
+    var searchValue = document.getElementById("searchInput").value.toLowerCase();
+    var postCards = document.querySelectorAll(".post-card");
+
+    var hasResults = false;
+
+    postCards.forEach(function(card) {
+        var img = card.querySelector("img");
+        var description = img.getAttribute("data-description").toLowerCase();
+
+        if (description.includes(searchValue)) {
+            card.style.display = "block";
+            hasResults = true;
+        } else {
+            card.style.display = "none";
+        }
+    })
+
+    var noResultsDiv = document.getElementById("noResultsMessage");
+    if (hasResults) {
+        noResultsDiv.style.display = "none";
+    } else {
+        noResultsDiv.style.display = "block";
+    }
+}
+document.getElementById("searchInput").addEventListener("input", postsText);
