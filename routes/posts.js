71cfb4a756f416abedde9eda5c24606956add7b4
noyush-ma <noya8657@gmail.com>
2026-08-27 15:11:43 +0300

    fix

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('./controllers/postController');
+const postController = require('../controllers/postController');
 
 router.post('/', postController.createPost);
 router.get('/', postController.getAllPosts);
