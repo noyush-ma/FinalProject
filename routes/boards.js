50a0697e60b11929ec413de479c50458ec052b1b
LotemFlower <lotem.parente@gmail.com>
2026-08-31 13:30:02 +0300

    Update boards.js

--- routes/boards.js
+++ routes/boards.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 const boardController = require('../controllers/boardController');
 
-// ניהול לוחות
+
 router.post('/', boardController.createBoard);
 router.get('/user/:userId', boardController.getUserBoards);
+router.get('/pins/:userId', boardController.getUserSavedPins);
 router.get('/save-status/:postId/:userId', boardController.getSaveStatus);
 router.get('/:id/pins', boardController.getBoardPins);
 router.get('/:id', boardController.getBoardById);
 router.put('/:id', boardController.updateBoard);
 router.delete('/:id', boardController.deleteBoard);
 
-// שמירה/הסרה של פוסטים מלוחות
-router.post('/save', boardController.savePin);
-router.post('/unsave', boardController.unsavePin);
+router.post('/save', boardController.savePin);            
+router.post('/add-to-board', boardController.addPinToBoard);   
+router.post('/remove-from-board', boardController.removeFromBoard); 
+router.post('/unsave', boardController.unsavePin);           
 
 module.exports = router;
