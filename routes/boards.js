6533423b98444eb4dc9ff89b19711ff5b7ab1e6e
LotemFlower <lotem.parente@gmail.com>
2026-09-02 09:51:39 +0300

    Update boards.js

--- routes/boards.js
+++ routes/boards.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const boardController = require('../controllers/boardController');
+
+
+router.post('/', boardController.createBoard);
 router.get('/user/:userId', boardController.getUserBoards);
 router.get('/pins/:userId', boardController.getUserSavedPins);
 router.get('/save-status/:postId/:userId', boardController.getSaveStatus);
