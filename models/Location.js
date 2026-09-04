a8cdf1b0726249a20ed8cfdd43bfa81efd8dea80
LotemFlower <lotem.parente@gmail.com>
2026-09-04 14:13:25 +0300

    Update Location.js

--- models/Location.js
+++ models/Location.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const locationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lng: { type: Number, required: true },
+  category: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Location', locationSchema);
