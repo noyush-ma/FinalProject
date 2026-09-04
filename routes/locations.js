f5796715ab8307227353138772c284e4996f7abb
LotemFlower <lotem.parente@gmail.com>
2026-09-04 14:14:09 +0300

    Update locations.js

--- routes/locations.js
+++ routes/locations.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Location = require('../models/Location');
 
+router.get('/', async (req, res) => {
+  try {
+    const locations = await Location.find({});
+    console.log('מיקומים שנשלחו מהשרת:', locations); 
+    res.json(locations);
+  } catch (err) {
+    console.error('שגיאה במסד הנתונים:', err.message); 
+    res.status(500).json({ message: 'שגיאה בטעינת המיקומים', error: err.message });
+  }
+});
+
+module.exports = router;
